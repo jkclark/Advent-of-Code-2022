8b64557fde91f18203928910f2b4d1f929fa5df7
Josh Clark <jclarktennis@gmail.com>
2022-12-16 16:14:53 +0100

    Added template solution file and files for problem 9.

---
+++ solution_09.py
@@ -0,0 +1,17 @@
+from utils import read_file
+
+
+def problem_09_1():
+    input_lines = read_file("inputs/09_1.txt")
+
+    return input_lines
+
+
+def problem_09_2():
+    input_lines = read_file("inputs/09_1.txt")
+
+    return input_lines
+
+
+print(f"Problem 09 - 1: {problem_09_1()}")
+print(f"Problem 09 - 2: {problem_09_2()}")
---
+++ solution_template.py
@@ -0,0 +1,17 @@
+from utils import read_file
+
+
+def problem_xx_1():
+    input_lines = read_file("inputs/xx_1.txt")
+
+    return input_lines
+
+
+def problem_xx_2():
+    input_lines = read_file("inputs/xx_1.txt")
+
+    return input_lines
+
+
+print(f"Problem xx - 1: {problem_xx_1()}")
+print(f"Problem xx - 2: {problem_xx_2()}")
