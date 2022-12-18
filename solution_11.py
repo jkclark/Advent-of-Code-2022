5d9abb5507c716075ee6b4ef0adb4d9257ecd92a
Josh Clark <jclarktennis@gmail.com>
2022-12-18 12:28:29 +0100

    Changed xx's to 11's in problem 11 files.

--- solution_11.py
+++ solution_11.py
@@ -1,17 +1,17 @@
 from utils import read_file
 
 
-def problem_xx_1():
-    input_lines = read_file("inputs/xx_1.txt")
+def problem_11_1():
+    input_lines = read_file("inputs/11_1.txt")
 
     raise NotImplementedError
 
 
-def problem_xx_2():
-    input_lines = read_file("inputs/xx_1.txt")
+def problem_11_2():
+    input_lines = read_file("inputs/11_1.txt")
 
     raise NotImplementedError
 
 
-print(f"Problem xx - 1: {problem_xx_1()}")
-print(f"Problem xx - 2: {problem_xx_2()}")
+print(f"Problem 11 - 1: {problem_11_1()}")
+print(f"Problem 11 - 2: {problem_11_2()}")
