38c268c6d141d4b624dd5c2299274f514c95fbbd
Josh Clark <jclarktennis@gmail.com>
2022-12-18 12:27:50 +0100

    Created files for problem 11.

---
+++ solution_11.py
@@ -0,0 +1,17 @@
+from utils import read_file
+
+
+def problem_xx_1():
+    input_lines = read_file("inputs/xx_1.txt")
+
+    raise NotImplementedError
+
+
+def problem_xx_2():
+    input_lines = read_file("inputs/xx_1.txt")
+
+    raise NotImplementedError
+
+
+print(f"Problem xx - 1: {problem_xx_1()}")
+print(f"Problem xx - 2: {problem_xx_2()}")
